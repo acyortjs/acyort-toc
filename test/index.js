38f82c6b159ee731a46b1ced201ba9cc749b0160
LoeiFy <LoeiFy@gmail.com>
2018-02-24 20:05:20 +0800

    module updated

--- test/index.js
+++ test/index.js
@@ -20,13 +20,11 @@ describe('toc', () => {
     this.timeout(5000)
     await acyort.build()
 
-    assert($('/posts/126817142.html').html() === `<html><head></head><body><ul>
-<li><a href="#&#x9884;&#x89C8;&#x5730;&#x5740;">&#x9884;&#x89C8;&#x5730;&#x5740;</a></li><li><a href="#&#x9879;&#x76EE;&#x5730;&#x5740;">&#x9879;&#x76EE;&#x5730;&#x5740;</a></li><li><a href="#&#x5982;&#x4F55;&#x5B89;&#x88C5;">&#x5982;&#x4F55;&#x5B89;&#x88C5;</a></li><li><a href="#&#x4F7F;&#x7528;">&#x4F7F;&#x7528;</a><ul>
-<li><a href="#&#x65B0;&#x5EFA;">&#x65B0;&#x5EFA;</a></li><li><a href="#&#x914D;&#x7F6E;&#x4F60;&#x7684;blog">&#x914D;&#x7F6E;&#x4F60;&#x7684; blog</a></li><li><a href="#&#x751F;&#x6210;&#x535A;&#x5BA2;">&#x751F;&#x6210;&#x535A;&#x5BA2;</a></li><li><a href="#&#x53D1;&#x5E03;&#x4F60;&#x7684;blog">&#x53D1;&#x5E03;&#x4F60;&#x7684; blog</a></li><li><a href="#&#x6700;&#x540E;">&#x6700;&#x540E;</a></li></ul>
-</li><li><a href="#&#x53E6;&#x4E00;&#x79CD;&#x5B89;&#x88C5;&#x65B9;&#x5F0F;">&#x53E6;&#x4E00;&#x79CD;&#x5B89;&#x88C5;&#x65B9;&#x5F0F;</a></li><li><a href="#&#x540D;&#x5B57;&#x6765;&#x6E90;">&#x540D;&#x5B57;&#x6765;&#x6E90;</a></li></ul>
+    assert($('/about/index.html').html() === `<html><head></head><body><ul>
+<li><a href="#about">ABOUT</a></li><li><a href="#skills">SKILLS</a></li><li><a href="#contact">CONTACT</a></li></ul>
 
 </body></html>`)
 
-    assert($('/posts/244718742.html').html() === `<html><head></head><body></body></html>`)
+    assert($('/time/index.html').html() === `<html><head></head><body></body></html>`)
   })
 })
--- toc.js
+++ toc.js
@@ -1,13 +1,19 @@
 const tocFn = require('markdown-toc')
 
 function toc({ raw }) {
-  const { markeder } = this
+  const { marker } = this
   const { content } = tocFn(raw)
+  const { headingIdFormater } = marker
 
-  let html = markeder.mark(content)
+  let html = marker.parse(content)
 
   const hrefs = html.match(/href="([^"]*")/g) || []
-  const hrefsifies = hrefs.map(href => decodeURIComponent(href.replace(/_|-/g, '')))
+  const hrefsifies = hrefs.map((href) => {
+    let s = href.split('href="')[1]
+    s = s.substring(0, s.length - 1)
+    s = headingIdFormater(s)
+    return `href="${s}"`
+  })
 
   hrefs.forEach((href, i) => {
     html = html.replace(href, hrefsifies[i])
